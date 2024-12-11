2f4bba6f65863c514797bc14dd4adbe4c5ebee84
Andre Freitas <andre.silvestre@vitta.me>
2024-12-11 14:14:15 -0300

    add complexObj

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { ComplexObj } from './domain/complexObj';
 
 @Injectable()
 export class AppService {
-
   /**
    * Aqui já é a classe service
    * @param greeting mensagem opcional
-   * @returns 
+   * @returns
    */
   getHello(greeting?: string): string {
     return 'Hello World!';
+  }
+
+  /**
+   * This class calculate the age from Person
+   * @param compleXObject 
+   * @returns a complexObject
+   */
+  getCompodoc(compleXObject: ComplexObj): ComplexObj {
+    return {
+      age:compleXObject.age,
+      name: compleXObject.name
+    }
   }
 }
---
+++ src/domain/complexObj.ts
@@ -0,0 +1,4 @@
+export class ComplexObj {
+    name:string
+    age:number
+}
